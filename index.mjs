f7d5fed871652d49fab2bd7cba97351568cd67d4
Stian Haklev <stian@minervaproject.com>
2020-02-17 11:16:53 +0100

    WIP

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import os from "os";
+import fs from "fs";
+import { exec } from "child_process";
+import https from "https";
+
+const homedir = os.homedir();
+const instapaperToken = JSON.parse(
+  fs.readFileSync(`${os.homedir()}/.instapaper-token.json`, "utf-8")
+);
+
+const postUrlToInstapaper = url => {
+  const apiUrl = `https://www.instapaper.com/api/add?url=${url}&username=${instapaperToken.username}&password={instapaperToken.password}`;
+  https.get(apiUrl);
+};
+
+exec("/usr/local/bin/pbpaste-html", (err, stdout, stderr) => {
+  const links = new RegExp(/href\s*=\s*(['"])(https?:\/\/.+?)\1/, "gi");
+  const urls = Array.from(stdout.matchAll(links));
+  urls.forEach(x => postUrlToInstapaper(x[2]));
+  console.log(`${urls.length} sent to Instapaper`);
+});
